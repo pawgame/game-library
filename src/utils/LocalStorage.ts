15247e5e6b1ad5a20b3ef362878cec2574b3eaed
daichangxin <daichangxin@gmail.com>
2020-08-24 17:04:50 +0800

    feat: add LocalStorage封装

---
+++ src/utils/LocalStorage.ts
@@ -0,0 +1,93 @@
+import {ObjectUtils} from "./ObjectUtils";
+
+type StorageDB = {
+    getItem: (key: string) => string;
+    setItem: (key: string, value: string) => void;
+    removeItem: (key: string) => void;
+}
+
+export class LocalStorage {
+    static readonly defaultInst = new LocalStorage('$pawgame');
+
+    private _data: any;
+    readonly mainKey: string;
+    private _db: StorageDB;
+
+    constructor(mainKey: string, db?: StorageDB) {
+        this.mainKey = mainKey;
+        this._db = db || localStorage;
+        this.initialize();
+    }
+
+    private initialize() {
+        try {
+            const strData = this._db.getItem(this.mainKey);
+            this._data = strData ? JSON.parse(strData) : {};
+        } catch (e) {
+            this._data = {};
+        }
+    }
+
+    /**
+     * TODO ADD CallLater
+     */
+    private invalidate(now?: boolean) {
+        this._db.setItem(this.mainKey, JSON.stringify(this._data));
+    }
+
+    getBool(key: string) {
+        return ObjectUtils.parseInt(this._data[key]) === 1;
+    }
+
+    setBool(key: string, v: boolean, now?: boolean) {
+        const num_v = v ? 1 : 0;
+        if (num_v === this._data[key]) return;
+        this._data[key] = num_v;
+        this.invalidate(now);
+    }
+
+    getStr(key: string) {
+        return this._data[key];
+    }
+
+    setStr(key: string, value: string, now?: boolean) {
+        if (this._data[key] == value) return;
+        this._data[key] = value;
+        this.invalidate(now);
+    }
+
+    getNum(key: string) {
+        return ObjectUtils.parseFloat(this._data[key]);
+    }
+
+    setNum(key: string, value: number, now?: boolean) {
+        value = ObjectUtils.parseFloat(value);
+        if (this._data[key] == value) return;
+        this._data[key] = value;
+        this.invalidate(now);
+    }
+
+    getObj(key: string) {
+        return this._data[key];
+    }
+
+    setObj(key: string, obj: any, now?: boolean) {
+        this._data[key] = obj;
+        if (!obj) {
+            delete this._data[key];
+        }
+        this.invalidate(now);
+    }
+
+    clearItem(key: string) {
+        this._data[key] = null;
+        delete this._data[key];
+        this.invalidate();
+    }
+
+    clearAll() {
+        this._data = {};
+        this._db.removeItem(this.mainKey);
+    }
+
+}
