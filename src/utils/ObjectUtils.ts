a21cfdedb20e39aaa14d42caad3066b080b28ab1
代长新 <daichangxin@gmail.com>
2021-01-18 17:26:57 +0800

    fix: toFixed error

--- src/utils/ObjectUtils.ts
+++ src/utils/ObjectUtils.ts
@@ -9,9 +9,19 @@ const parseFloat = (v: any) => +v || 0;
 const parseInt = (v: any) => ~~v;
 
 /**
- * 与原生toFixed不同的是，如果是2.10则会变成2.1
+ * 与原生toFixed不同的是，如果是2.10则会变成2.1; 1.16是1.1而不是1.2
  */
-const toFixed = (num: number, fractionDigits?: number) => parseFloat(num.toFixed(fractionDigits));
+const toFixed = (num: number, fractionDigits = 0) => {
+    const dotIndex = `${num}`.indexOf('.');
+    if (dotIndex === -1) {
+        return num;
+    }
+    const fixedNum = `${num}`.substr(
+        0,
+        fractionDigits > 0 ? dotIndex + fractionDigits + 1 : dotIndex,
+    );
+    return parseFloat(fixedNum);
+};
 
 export const ObjectUtils = {
     parseFloat,
